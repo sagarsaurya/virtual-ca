89173b87b8380d4beceeb986d9d6a6799ac8e134
Sagar Pathak <sagarsaurya0@gmail.com>
2026-06-24 15:21:34 +0530

    Fix model: meta-llama/llama-3.1-8b-instruct (confirmed working)

--- openrouter_client.py
+++ openrouter_client.py
@@ -6,7 +6,7 @@ import os
 from openai import OpenAI
 
 OPENROUTER_API_KEY = os.environ.get('OPENROUTER_API_KEY', '')
-MODEL = 'meta-llama/llama-3.1-8b-instruct:free'
+MODEL = 'meta-llama/llama-3.1-8b-instruct'
 
 import sys
 print(f'[OpenRouter] key set={bool(OPENROUTER_API_KEY)} model={MODEL}', file=sys.stderr, flush=True)
